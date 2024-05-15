e678794b389933a95f1115da59e3bcffa2f2ee1e
Eduxplorer <eduardo.ssilva77@senacsp.edu.br>
2024-05-15 16:27:12 -0300

    Função de renderização

--- Fundamentos/vite-project/src/componentes/EventoClick.jsx
+++ Fundamentos/vite-project/src/componentes/EventoClick.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
 const EventoClick = () => {
+
+    // 8 - Função com HandleClick
+
+    const handleClick = () => {
+        console.log('Handleclick executou!')
+    }
+
+    // 9 - Função de renderização
+
+    const renderizando = (x) => {
+        if (x) {
+            return <h1>Renderizando X</h1>
+        } else {
+            return <h1>Renderizando Y</h1>
+        }
+    }
+
     return (
         <div>
-            <button onClick={() => console.log("teste clique")}>Clique Aqui!</button>
+            <div>
+                <button onClick={() => console.log("teste clique")}>Clique Aqui!</button>
+            </div>
+
+            {/*Chamando Função com handleClick */}
+            <div>
+                <button onClick={handleClick}>Este botão tem uma função</button>
+            </div>
+
+            {/*Retornando a função com renderização */}
+            <div>
+                {renderizando(true)}
+                {renderizando()}
+            </div>
+
         </div>
     )
 }
