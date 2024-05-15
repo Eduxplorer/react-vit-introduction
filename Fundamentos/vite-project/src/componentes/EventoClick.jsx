2158a30e5b9243ab46ac19c3de77ecf3b26fd357
Eduxplorer <eduardo.ssilva77@senacsp.edu.br>
2024-05-15 15:56:38 -0300

    Evento de clique feito

--- Fundamentos/vite-project/src/componentes/EventoClick.jsx
+++ Fundamentos/vite-project/src/componentes/EventoClick.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const EventoClick = () => {
     return (
         <div>
-            <button onClick>Clique Aqui</button>
+            <button onClick={() => console.log("teste clique")}>Clique Aqui!</button>
         </div>
     )
 }
 
-export default EventoClick
+export default EventoClick;
